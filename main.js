6d3f36f014c611d97f37c0d1750fd4141a8a0601
dragon <abdallahdragon72@gmail.com>
2024-03-23 03:57:01 +0200

    Uploud Files

--- main.js
+++ main.js
@@ -66,20 +66,37 @@ function createPost(
         />
       </svg>
       <span>(${commentsCount}) Comments</span>
+      <span id="post-tags${post.id}">
+        <button class="btn btn-sm rounded-5" style="background-color: gray;color: #fff;">
+          Policy
+        </button>
+      </span>
     </div>
   </div>
 </div>
 `;
   document.getElementById("posts").innerHTML += content;
+  // ADD TAGS
+  let currentPost = `post-tags${post.id}`;
+  document.getElementById(currentPost).innerHTML = "";
+  for (tag of post.tags) {
+    console.log(tag.name);
+    let tagsContent = `
+        <button class="btn btn-sm rounded-5" style="background-color: gray;color: #fff;">
+          ${tag.name}
+        </button>
+    `;
+    document.getElementById(currentPost).innerHTML += tagsContent;
+  }
 }
 /*
 =================================================================
 */
 // Login Button ====================
-const loginSubmitBtn = document.getElementById("login-submit-btn");
-loginSubmitBtn.addEventListener("click", () => {
-  loginBtnClicked();
-});
+// const loginSubmitBtn = document.getElementById("login-submit-btn");
+// loginSubmitBtn.addEventListener("click", () => {
+//   loginBtnClicked();
+// });
 function loginBtnClicked() {
   const userNameInput = document.getElementById("username-input").value;
   const passwordInput = document.getElementById("password-input").value;
@@ -98,11 +115,48 @@ function loginBtnClicked() {
         document.getElementById("login-modal")
       ).hide();
       setupUI();
-      showSuccessAlert("logged In Successfully");
+      showAlert("logged In Successfully");
     })
     .catch((error) => {
       console.log(error);
     });
+}
+// ==================
+function registerBtnClicked() {
+  const nameInput = document.getElementById("register-name-input").value;
+  const userNameInput = document.getElementById(
+    "register-username-input"
+  ).value;
+  const passwordInput = document.getElementById(
+    "register-password-input"
+  ).value;
+  const params = {
+    username: userNameInput,
+    password: passwordInput,
+    name: nameInput,
+  };
+  axios
+    .post(`${baseUrl}/register`, params)
+    .then((response) => {
+      console.log(response.data);
+      // localStorage.setItem("token", response.data.token);
+      // localStorage.setItem("user", JSON.stringify(response.data.user));
+      // to close the Modal After Login
+      bootstrap.Modal.getInstance(
+        document.getElementById("register-modal")
+      ).hide();
+      setupUI();
+      showAlert("New User Register Successfully");
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+      const errorMessage = error.response.data.message;
+      showAlert(errorMessage, "danger");
+    });
+}
+// ==================
+function createNewPostClicked() {
+  //
 }
 // ===================================
 // Logout Button ====================
@@ -110,7 +164,7 @@ function logout() {
   localStorage.removeItem("token");
   localStorage.removeItem("user");
   setupUI();
-  showSuccessAlert("logged Out Successfully");
+  showAlert("logged Out Successfully");
 }
 // ===================================
 
@@ -120,14 +174,17 @@ function setupUI() {
   const loginBtn = document.getElementById("login-btn");
   const registerBtn = document.getElementById("register-btn");
   const logoutBtn = document.getElementById("logout-btn");
+  const addBtn = document.getElementById("add-btn");
   if (token == null) {
-    loginBtn.style.visibility = "visible";
-    registerBtn.style.visibility = "visible";
+    loginBtn.style.display = "block";
+    registerBtn.style.display = "block";
     logoutBtn.style.display = "none";
+    addBtn.style.display = "none";
   } else {
-    loginBtn.style.visibility = "hidden";
-    registerBtn.style.visibility = "hidden";
+    loginBtn.style.display = "none";
+    registerBtn.style.display = "none";
     logoutBtn.style.display = "block";
+    addBtn.style.display = "block";
   }
 }
 
@@ -135,7 +192,7 @@ function setupUI() {
 // =============
 // =============
 // =============
-function showSuccessAlert(alertMsg) {
+function showAlert(alertMsg, status = "success") {
   const alertPlaceholder = document.getElementById("success-alert");
 
   const alert = (message, type) => {
@@ -150,7 +207,7 @@ function showSuccessAlert(alertMsg) {
 
     alertPlaceholder.append(wrapper);
   };
-  alert(alertMsg, "success");
+  alert(alertMsg, status);
   // \Todo: Hide THe Alert
   setTimeout(() => {
     const hideAlert = bootstrap.Alert.getOrCreateInstance("#success-alert");
